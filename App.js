87e95804bc3829d6e40fdd55001fc5c43a3f0564
lkacoy <lkacoyannakis@gmail.com>
2018-06-25 19:58:58 -0400

    lighten status bar

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import FixedHeader from './elements/FixedHeader';
 
 export default class App extends React.Component {
   render() {
     return (
       <View>
+          <StatusBar barStyle="light-content"/>
           <FixedHeader/>
       </View>
     );
