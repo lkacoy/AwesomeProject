55aa26ac143ee2454b729c398561a391771c3030
lkacoy <lkacoyannakis@gmail.com>
2018-06-25 19:57:29 -0400

    remove style from view

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import FixedHeader from './elements/FixedHeader';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View>
           <FixedHeader/>
       </View>
     );
