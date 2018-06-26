c2ab27e45e6b4f7d5333f8cdf3ecc6b947d852a8
lkacoy <lkacoyannakis@gmail.com>
2018-06-25 21:47:02 -0400

    add updates to picker

--- elements/QuestionTypePicker.js
+++ elements/QuestionTypePicker.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Picker} from 'react-native';
+import {Picker, Text, View} from 'react-native';
 
 export default class QuestionTypePicker extends Component {
     constructor(props) {
@@ -9,15 +9,18 @@ export default class QuestionTypePicker extends Component {
 
     render() {
         return (
-            <Picker
-                selectedValue={this.state.questionType}
-                onValueChange={(itemValue, itemIndex) =>
-                    this.setState({questionType: itemValue})}>
-                <Picker.Item value="MC" label="Multiple choice" />
-                <Picker.Item value="ES" label="Essay" />
-                <Picker.Item value="TF" label="True or false" />
-                <Picker.Item value="FB" label="Fill in the blanks" />
-            </Picker>
+            <View>
+                <Picker
+                    selectedValue={this.state.questionType}
+                    onValueChange={(itemValue, itemIndex) =>
+                        this.setState({questionType: itemValue})}>
+                    <Picker.Item value="MC" label="Multiple choice" />
+                    <Picker.Item value="ES" label="Essay" />
+                    <Picker.Item value="TF" label="True or false" />
+                    <Picker.Item value="FB" label="Fill in the blanks" />
+                </Picker>
+                <Text>{this.state.questionType}</Text>
+            </View>
         )
     }
 }
