b65e1f23a058bca783ebe74d7acc69cdd0661b42
lkacoy <lkacoyannakis@gmail.com>
2018-06-25 20:04:17 -0400

    add additional text sizes

--- elements/TextHeadings.js
+++ elements/TextHeadings.js
@@ -4,5 +4,8 @@ import { Text } from 'react-native-elements'
 const TextHeadings = () => (
     <View>
         <Text h1>Welcome!</Text>
+        <Text h2>Heading size 2</Text>
+        <Text h3>Heading size 3</Text>
+        <Text h4>Heading size 4</Text>
     </View>)
 export default TextHeadings
