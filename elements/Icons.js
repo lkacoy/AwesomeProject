5d23b30c4317f9b4722beabc1619d580518512dc
lkacoy <lkacoyannakis@gmail.com>
2018-06-25 20:06:20 -0400

    add icons

---
+++ elements/Icons.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { View, Alert } from 'react-native'
+import { Icon } from 'react-native-elements'
+const Icons = () => (
+    <View>
+    </View>
+)
+export default Icons
