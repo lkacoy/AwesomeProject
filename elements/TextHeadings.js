e13cd75dec12ea58b35135497b2bb0a96cc8204f
lkacoy <lkacoyannakis@gmail.com>
2018-06-25 20:05:55 -0400

    add divider

--- elements/TextHeadings.js
+++ elements/TextHeadings.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { View } from 'react-native'
-import { Text } from 'react-native-elements'
+import { Text, Divider } from 'react-native-elements'
 const TextHeadings = () => (
     <View>
         <Text h1>Welcome!</Text>
         <Text h2>Heading size 2</Text>
         <Text h3>Heading size 3</Text>
         <Text h4>Heading size 4</Text>
+        <Divider style={{backgroundColor: blue}}/>
         <Text>
             Lorem ipsum dolor sit amet,
             consectetur adipiscing elit,
