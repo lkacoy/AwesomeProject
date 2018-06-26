1ea638b297ab78938b3bd688a3f343b62d0255c6
lkacoy <lkacoyannakis@gmail.com>
2018-06-25 20:29:52 -0400

    add listitems dynamically

--- elements/Exam.js
+++ elements/Exam.js
@@ -2,15 +2,27 @@ import React, {Component} from 'react';
 import {View} from 'react-native';
 import {ListItem, Text} from 'react-native-elements';
 
+const questions = [
+    {	title: 'Question 1', subtitle: 'Multiple choice',
+        icon: 'list'},
+    {	title: 'Question 2', subtitle: 'Fill-in the blanks',
+        icon: 'code'},
+    {	title: 'Question 3', subtitle: 'True or false',
+        icon: 'check'},
+    {	title: 'Question 4', subtitle: 'Essay',
+        icon: 'subject'}];
+
 export default class Exam extends Component {
     render() {
         return (
             <View stye={{padding: 15}}>
                 <Text h2>Lists</Text>
-                <ListItem title="Question 1" leftIcon={{name:"list"}} subtitle="Multiple Choice"/>
-                <ListItem title="Question 2" leftIcon={{name:"code"}} subtitle="Fill-in the blanks"/>
-                <ListItem title="Question 3"/>
-                <ListItem title="Question 4"/>
+                {questions.map((question, index) => (
+                    <ListItem key={index}
+                              title={question.title}
+                              subtitle={question.subtitle}
+                              leftIcon={name: question.icon}/>
+                ))}
             </View>
         )
     }
