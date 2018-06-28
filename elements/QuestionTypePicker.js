34ee1fac6829d1f7c28e00b0ec30894f78011efa
lkacoy <lkacoyannakis@gmail.com>
2018-06-27 22:06:03 -0400

    make question picker dynamic

--- elements/QuestionTypePicker.js
+++ elements/QuestionTypePicker.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
 import {Picker, Text, View} from 'react-native';
+import MultipleChoiceQuestionEditor from "./MultipleChoiceQuestionEditor";
+import TrueFalseQuestionEditor from "./TrueFalseQuestionEditor";
+import EssayQuestionEditor from "./EssayQuestionEditor";
+import FillInBlanksQuestionEditor from "./FillInBlanksQuestionEditor";
 
 export default class QuestionTypePicker extends Component {
     constructor(props) {
         super(props);
         this.state = {questionType: 'MC'}
+        this.displayQuestionType = this.displayQuestionType.bind(this);
     }
 
     render() {
@@ -19,8 +24,28 @@ export default class QuestionTypePicker extends Component {
                     <Picker.Item value="TF" label="True or false" />
                     <Picker.Item value="FB" label="Fill in the blanks" />
                 </Picker>
-                <Text>{this.state.questionType}</Text>
+                {this.displayQuestionType()}
             </View>
         )
+    }
+
+    displayQuestionType() {
+        if (this.state.questionType == 'MC') {
+            return (
+                <MultipleChoiceQuestionEditor/>
+            )
+        } else if (this.state.questionType == 'ES') {
+            return (
+                <EssayQuestionEditor/>
+            )
+        } else if (this.state.questionType == 'TF') {
+            return (
+                <TrueFalseQuestionEditor/>
+            )
+        } else {
+            return (
+                <FillInBlanksQuestionEditor/>
+            )
+        }
     }
 }
