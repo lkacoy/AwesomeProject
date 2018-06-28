2d2fac16b55b6509308a79d9d4b10f3738151429
lkacoy <lkacoyannakis@gmail.com>
2018-06-27 22:05:51 -0400

    add some stuff to the editors

--- elements/EssayQuestionEditor.js
+++ elements/EssayQuestionEditor.js
@@ -1,4 +1,6 @@
-import {Component} from "react";
+import React, {Component} from 'react';
+import {View} from 'react-native';
+import {CheckBox, FormLabel, FormInput, FormValidationMessage, Text, Button} from 'react-native-elements';
 
 export default class EssayQuestionEditor extends Component {
 
@@ -7,8 +9,44 @@ export default class EssayQuestionEditor extends Component {
         this.state = {
             title: "",
             description: "",
-            points: 0,
-            isTrue: true
+            points: 0
         }
+    }
+
+
+    formUpdate(newState) {
+        this.setState(newState);
+    }
+
+    render() {
+        return (
+            <View>
+                <Text h3>Editor - Essay</Text>
+                <FormLabel>Title</FormLabel>
+                <FormInput onChangeText={
+                    text => this.formUpdate({title: text}) }/>
+                <FormValidationMessage>
+                    Title is required
+                </FormValidationMessage>
+                <FormLabel>Description</FormLabel>
+                <FormInput onChangeText={
+                    text => this.formUpdate({description: text}) }/>
+                <FormValidationMessage>
+                    Description is required
+                </FormValidationMessage>
+
+
+                <Button	backgroundColor="green"
+                           color="white"
+                           title="Save"/>
+                <Button	backgroundColor="red"
+                           color="white"
+                           title="Cancel"/>
+
+                <Text h3>Preview</Text>
+                <Text h2>{this.state.title}</Text>
+                <Text>{this.state.description}</Text>
+            </View>
+        )
     }
 }
