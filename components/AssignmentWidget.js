672c63f73790f587a5b3a7d128b1e4c2a2124148
lkacoy <lkacoyannakis@gmail.com>
2018-06-28 00:02:45 -0400

    add things to assignment widget

--- components/AssignmentWidget.js
+++ components/AssignmentWidget.js
@@ -1,16 +1,29 @@
 import React, {Component} from 'react'
 import {View} from 'react-native'
-import {Text, ListItem} from 'react-native-elements'
+import {Text, Button} from 'react-native-elements'
 import ModuleList from "./ModuleList";
+
+const Preview = () => (
+    <View>
+        <Text h1>Preview</Text>
+        <Button title="Cancel"/>
+        <Button title="Submit"/>
+    </View>
+);
 
 class AssignmentWidget extends Component {
 
+    static navigationOptions = {title: 'Assignments'}
     constructor(props) {
         super(props)
         this.state = {
-            widgets: [],
-            lessonId: 1,
+            id: '',
+            title: '',
+            description: '',
+            points: 0,
+            lessonId: 1
         }
+
     }
 
     componentDidMount() {
@@ -20,11 +33,23 @@ class AssignmentWidget extends Component {
     }
 
     render() {
+        console.log(this.state.preview);
         return (
-            <View stye={{padding: 15}}>
-                <Text h2>Lists</Text>
+            <View style={{padding: 15}}>
+                <Text h4>Title</Text>
+                <Text h4>Description</Text>
+                <Text h4>Points</Text>
+
+                <Text h1>Preview</Text>
+                <Text>Assignment: {this.state.id} - {this.state.title}</Text>
+                <Text>{this.state.points} pts</Text>
+                <Text>{this.state.description}</Text>
+                <Button title="Cancel"/>
+                <Button title="Submit"/>
             </View>
         )
     }
+
+
 }
 export default AssignmentWidget
